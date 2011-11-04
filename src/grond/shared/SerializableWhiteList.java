cf28ff9b780e7747030fb2d2288df99c443c6497
ArtemGr <artemciy@gmail.com>
2011-11-04 19:29:53 +0300

    RPC refactorings

---
+++ src/grond/shared/SerializableWhiteList.java
@@ -0,0 +1,11 @@
+package grond.shared;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+/**
+ * Extend the list of serializable classes.<br>
+ * https://groups.google.com/group/google-web-toolkit/browse_thread/thread/9eb513e449be3940/
+ */
+public class SerializableWhiteList implements IsSerializable {
+  @SuppressWarnings("unused") private Double aDouble;
+}
